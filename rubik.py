a9d9d5c5c0ab07038f19f92008500601dc146d40
Jason Hall <jasonbrianhall@gmail.com>
2025-02-04 10:42:12 -0500

    Initial commit

--- rubik.py
+++ rubik.py
@@ -1,135 +1,141 @@
-import pygame
-from pygame.locals import *
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QOpenGLWidget
+from PyQt5.QtCore import Qt
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import numpy as np
 
-pygame.init()
-info = pygame.display.Info()
-display = (info.current_w, info.current_h)
-pygame.display.set_mode(display, DOUBLEBUF | OPENGL | FULLSCREEN)
-gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-glTranslatef(0.0, 0.0, -15)
+class GLWidget(QOpenGLWidget):
+    def __init__(self, parent=None):
+        super(GLWidget, self).__init__(parent)
+        self.cube = RubiksCube()
 
-class Cube:
-    def __init__(self, position, color):
-        self.position = position
-        self.color = color
-        self.vertices = [
-            [1, -1, -1], [1, 1, -1], [-1, 1, -1], [-1, -1, -1],
-            [1, -1, 1], [1, 1, 1], [-1, -1, 1], [-1, 1, 1]
-        ]
-        self.surfaces = [
-            [0, 1, 2, 3], [3, 2, 7, 6], [6, 7, 5, 4],
-            [4, 5, 1, 0], [1, 5, 7, 2], [4, 0, 3, 6]
-        ]
-        self.edges = [
-            (0,1), (1,2), (2,3), (3,0),
-            (4,5), (5,6), (6,7), (7,4),
-            (0,4), (1,5), (2,6), (3,7)
-        ]
+    def initializeGL(self):
+        glEnable(GL_DEPTH_TEST)
+        glClearColor(0.9, 0.9, 0.9, 1.0)
 
-    def draw(self):
-        glPushMatrix()
-        glTranslatef(*self.position)
-        
-        # Draw faces
-        glBegin(GL_QUADS)
-        for surface in self.surfaces:
-            glColor3fv(self.color)
-            for vertex in surface:
-                glVertex3fv(np.array(self.vertices[vertex]) * 0.45)  # Slightly smaller cubes
-        glEnd()
-        
-        # Draw black edges
-        glLineWidth(2.0)
-        glBegin(GL_LINES)
-        glColor3f(0, 0, 0)
-        for edge in self.edges:
-            for vertex in edge:
-                glVertex3fv(np.array(self.vertices[vertex]) * 0.45)
-        glEnd()
-        
-        glPopMatrix()
+    def resizeGL(self, w, h):
+        glViewport(0, 0, w, h)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45, w/h, 0.1, 50.0)
+        glTranslatef(0.0, 0.0, -15)
+
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        self.cube.draw()
+        self.update()
 
 class RubiksCube:
     def __init__(self):
+        self.rotation = [30, 45, 0]
         self.cubes = []
-        self.rotation = [30, 45, 0]  # Initial rotation for isometric view
-        self.colors = {
-            'white': (1, 1, 1),
-            'yellow': (1, 1, 0),
-            'red': (1, 0, 0),
-            'orange': (1, 0.5, 0),
-            'blue': (0, 0, 1),
-            'green': (0, 1, 0)
-        }
         self._initialize_cubes()
-        self._apply_initial_rotation()
 
     def _initialize_cubes(self):
         for x in [-1, 0, 1]:
             for y in [-1, 0, 1]:
                 for z in [-1, 0, 1]:
-                    pos = (x, y, z)
-                    color = self._get_cube_color(pos)
-                    self.cubes.append(Cube(pos, color))
-
-    def _apply_initial_rotation(self):
-        glRotatef(self.rotation[0], 1, 0, 0)
-        glRotatef(self.rotation[1], 0, 1, 0)
-        glRotatef(self.rotation[2], 0, 0, 1)
-
-    def _get_cube_color(self, pos):
-        x, y, z = pos
-        if z == 1:  # Front face
-            return self.colors['white']
-        elif z == -1:  # Back face
-            return self.colors['yellow']
-        elif y == 1:  # Top face
-            return self.colors['red']
-        elif y == -1:  # Bottom face
-            return self.colors['orange']
-        elif x == -1:  # Left face
-            return self.colors['blue']
-        elif x == 1:  # Right face
-            return self.colors['green']
-        return (0.8, 0.8, 0.8)  # Center cubes
-
-    def rotate(self, axis, angle):
-        self.rotation[axis] += angle
-        glRotatef(angle, axis == 0, axis == 1, axis == 2)
+                    self.cubes.append((x, y, z))
 
     def draw(self):
-        glClearColor(0.9, 0.9, 0.9, 1)  # Light gray background
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        for cube in self.cubes:
-            cube.draw()
+        glRotatef(self.rotation[0], 1, 0, 0)
+        glRotatef(self.rotation[1], 0, 1, 0)
+        
+        for pos in self.cubes:
+            x, y, z = pos
+            glPushMatrix()
+            glTranslatef(x, y, z)
+            self.draw_cube()
+            glPopMatrix()
+    
+    def draw_cube(self):
+        vertices = [
+            [0.4, 0.4, 0.4], [-0.4, 0.4, 0.4], [-0.4, -0.4, 0.4], [0.4, -0.4, 0.4],
+            [0.4, 0.4, -0.4], [-0.4, 0.4, -0.4], [-0.4, -0.4, -0.4], [0.4, -0.4, -0.4]
+        ]
+        
+        faces = [
+            [0, 1, 2, 3], [5, 4, 7, 6], [4, 0, 3, 7],
+            [1, 5, 6, 2], [4, 5, 1, 0], [3, 2, 6, 7]
+        ]
+        
+        glBegin(GL_QUADS)
+        glColor3f(1, 1, 1)
+        for face in faces:
+            for vertex in face:
+                glVertex3fv(vertices[vertex])
+        glEnd()
+        
+        glColor3f(0, 0, 0)
+        glLineWidth(2.0)
+        glBegin(GL_LINES)
+        edges = [
+            (0,1), (1,2), (2,3), (3,0),
+            (4,5), (5,6), (6,7), (7,4),
+            (0,4), (1,5), (2,6), (3,7)
+        ]
+        for edge in edges:
+            for vertex in edge:
+                glVertex3fv(vertices[vertex])
+        glEnd()
+
+class RubiksWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Rubiks Cube')
+        self.setGeometry(100, 100, 1200, 800)
+        
+        main_widget = QWidget()
+        self.setCentralWidget(main_widget)
+        layout = QHBoxLayout(main_widget)
+        
+        self.gl_widget = GLWidget()
+        layout.addWidget(self.gl_widget)
+        
+        controls = QWidget()
+        controls_layout = QVBoxLayout(controls)
+        
+        nav_layout = QHBoxLayout()
+        self.up_btn = QPushButton('↑')
+        self.down_btn = QPushButton('↓')
+        self.left_btn = QPushButton('←')
+        self.right_btn = QPushButton('→')
+        
+        for btn in [self.up_btn, self.down_btn, self.left_btn, self.right_btn]:
+            btn.setFixedSize(60, 60)
+            btn.setStyleSheet('font-size: 24px;')
+        
+        nav_layout.addWidget(self.left_btn)
+        nav_layout.addWidget(self.right_btn)
+        controls_layout.addWidget(self.up_btn, alignment=Qt.AlignHCenter)
+        controls_layout.addLayout(nav_layout)
+        controls_layout.addWidget(self.down_btn, alignment=Qt.AlignHCenter)
+        
+        colors = [
+            ('Red', 'background-color: red'),
+            ('Yellow', 'background-color: yellow'),
+            ('Green', 'background-color: green'),
+            ('Blue', 'background-color: blue'),
+            ('Orange', 'background-color: orange'),
+            ('White', 'background-color: white')
+        ]
+        
+        for name, style in colors:
+            btn = QPushButton()
+            btn.setFixedSize(100, 100)
+            btn.setStyleSheet(f'{style}; border: 2px solid black;')
+            controls_layout.addWidget(btn)
+        
+        layout.addWidget(controls)
 
 def main():
-    rubiks_cube = RubiksCube()
-    clock = pygame.time.Clock()
-    rotation_speed = 5
+    app = QApplication(sys.argv)
+    window = RubiksWindow()
+    window.show()
+    sys.exit(app.exec_())
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                pygame.quit()
-                return
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    rubiks_cube.rotate(1, -rotation_speed)
-                elif event.key == pygame.K_RIGHT:
-                    rubiks_cube.rotate(1, rotation_speed)
-                elif event.key == pygame.K_UP:
-                    rubiks_cube.rotate(0, -rotation_speed)
-                elif event.key == pygame.K_DOWN:
-                    rubiks_cube.rotate(0, rotation_speed)
-
-        glEnable(GL_DEPTH_TEST)
-        rubiks_cube.draw()
-        pygame.display.flip()
-        clock.tick(60)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
